c32f2f72885746f0479d4637dfc6636b9d7b5aa0
VirgilliaYeala <virgilliayeala@ui.ac.id>
2023-09-12 20:02:28 +0700

    done with tugas 2

--- main/tests.py
+++ main/tests.py
@@ -1,6 +1,11 @@
 from django.test import TestCase, Client
+from main.models import Product
 
 class mainTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Product.objects.create(brand = "brand", name="nama", amount=2, description="test", price ="Rupiah")
+
     def test_main_url_is_exist(self):
         response = Client().get('/main/')
         self.assertEqual(response.status_code, 200)
@@ -8,3 +13,28 @@ class mainTest(TestCase):
     def test_main_using_main_template(self):
         response = Client().get('/main/')
         self.assertTemplateUsed(response, 'main.html')
+    
+    def test_brand_product(self):
+        buku = Product.objects.get(id=1)
+        field_label = buku._meta.get_field('brand').verbose_name
+        self.assertEqual(field_label, 'brand')
+
+    def test_name_product(self):
+        buku = Product.objects.get(id=1)
+        field_label = buku._meta.get_field('name').verbose_name
+        self.assertEqual(field_label, 'name')
+
+    def test_description_product(self):
+        buku = Product.objects.get(id=1)
+        field_label = buku._meta.get_field('description').verbose_name
+        self.assertEqual(field_label, 'description')
+    
+    def test_amount_product(self):
+        buku = Product.objects.get(id=1)
+        field_label = buku._meta.get_field('amount').verbose_name
+        self.assertEqual(field_label, 'amount')
+
+    def test_price_product(self):
+        buku = Product.objects.get(id=1)
+        field_label = buku._meta.get_field('price').verbose_name
+        self.assertEqual(field_label, 'price')
--- main/views.py
+++ main/views.py
@@ -2,8 +2,34 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'name': 'Virgillia Yeala Prabowo',
-        'class': 'PBP E'
+      'toner' : {
+        'brand' : "AVOSKIN -",
+        'name' : "Miraculous Retinol Toner",
+        'amount' : "3",
+        'description' : "mencerahkan dan meratakan tekstur kulit.",
+        'price' : "Rp 135.000"
+      },
+      'moisturizer' : {
+        'brand' : "Somethinc -",
+        'name' : "Ceramic Skin Saviour",
+        'amount' : "5",
+        'description' : "menghidrasi dan memperkuat kulit.",
+        'price': "Rp 186.000"
+      },
+      'serum' : {
+        'brand' : "AVOSKIN -",
+        'name' : "Miraculous Refining Serum",
+        'amount' : "11",
+        'description' : "mengeksfoliasi dan mencerahkan kulit.",
+        'price' : "Rp 110.000"
+      },
+      'sunscreen' : {
+        'brand' : "Somethinc -",
+        'name' : "Holyshield!",
+        'amount' : "12",
+        'description' : "memproteksi kulit dari panas matahari",
+        'price' : "Rp 53.900"
+      }
     }
 
     return render(request, "main.html", context)
