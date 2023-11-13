cebf51943983dd65cc5db1ddd134bdfcedd43cbc
VirgilliaYeala <virgilliayeala@ui.ac.id>
2023-11-13 15:20:52 +0700

    ngepush ulang keperluan deploy django

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user,  delete_product_ajax, add_stock, sub_stock, get_product_json, add_product_ajax
+from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id
+from main.views import show_json_by_id, register, login_user, logout_user,  delete_product_ajax, add_stock, sub_stock, get_product_json, add_product_ajax
 
 app_name = 'main'
 
