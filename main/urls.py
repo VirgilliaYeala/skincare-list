71b947e6395bb5613587108dfebab33e9cd84fa4
VirgilliaYeala <virgilliayeala@ui.ac.id>
2023-11-21 08:52:05 +0700

    deploy Tugas 9: menambahkan autentikasi Django

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id
+from main.views import create_product_flutter, show_main, create_product, show_xml, show_json, show_xml_by_id
 from main.views import show_json_by_id, register, login_user, logout_user,  delete_product_ajax, add_stock, sub_stock, get_product_json, add_product_ajax
 
 app_name = 'main'
@@ -18,5 +18,6 @@ urlpatterns = [
     path('add_stock/<int:id>/', add_stock, name='add_stock'),
     path('sub_stock/<int:id>/', sub_stock, name='sub_stock'),
     path('get-product/', get_product_json, name='get_product_json'),
-    path('create-ajax/', add_product_ajax, name='add_product_ajax')
+    path('create-ajax/', add_product_ajax, name='add_product_ajax'),
+    path('create-flutter/', create_product_flutter, name='create_product_flutter'),
 ]
